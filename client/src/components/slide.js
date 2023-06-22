7bfc079762d8fec39b41361f48dcf597e4c1549d
casey <caseyochieng46@gmail.com>
2023-06-22 21:38:59 +0300

    added pics for slide

---
+++ client/src/components/slide.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../components/css/slide.css'
+function Slide() {
+    return (
+        <div className='img-slider'>
+            <div className='slide'>
+                <img src='/images/ark-youth-church.jpg' alt='Ark Youth Church' />
+                <div className='info'>
+
+                </div>
+
+            </div>
+        </div>
+    )
+
+}
+export default Slide
