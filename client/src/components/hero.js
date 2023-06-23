f05dfe3353a7104a8d7a6c10c8e93a9e08035cbf
casey <caseyochieng46@gmail.com>
2023-06-23 19:17:45 +0300

    added slide autoplay

--- client/src/components/hero.js
+++ client/src/components/hero.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import '../components/css/hero.css'
+import Slide from '../components/slide'
 function Hero() {
 
     return (
         <section className='hero'>
+        <Slide/>
             <div className="glitch-wrapper">
                 <div className="glitch" data-glitch="ARK YOUTH CHURCH">ARK YOUTH CHURCH</div>
             </div>
