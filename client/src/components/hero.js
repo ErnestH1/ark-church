3624cd24f40e35ce9cd671552504a9a60eaf474c
casey <caseyochieng46@gmail.com>
2023-07-03 18:38:39 +0300

    just del slide

--- client/src/components/hero.js
+++ client/src/components/hero.js
@@ -4,13 +4,17 @@ import Slide from '../components/slide'
 function Hero() {
 
     return (
-        <section className='hero'>
-        <Slide/>
-            <div className="glitch-wrapper">
-                <div className="glitch" data-glitch="ARK YOUTH CHURCH">ARK YOUTH CHURCH</div>
-            </div>
-        </section>
+        <>
+
+        </>
     )
 }
 
 export default Hero
+
+{/* <section className='hero'> */ }
+{/* <Slide /> */ }
+{/* <div className="glitch-wrapper"> */ }
+{/* <div className="glitch" data-glitch="ARK YOUTH CHURCH">ARK YOUTH CHURCH</div> */ }
+// </div>
+// </section>
