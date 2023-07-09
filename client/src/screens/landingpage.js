2b19c9bd54e44e654d4a79d098e49c9438375736
casey <caseyochieng46@gmail.com>
2023-07-10 00:43:40 +0300

    added login and signup all bugs fixed here

--- client/src/screens/landingpage.js
+++ client/src/screens/landingpage.js
@@ -2,11 +2,13 @@ import React from 'react'
 import '../components/css/landing.css'
 import Hero from '../components/hero'
 import Navbar from '../components/navbar'
+import Footer from '../components/footer'
 function Landingpage() {
     return (
         <>
         <Navbar/>
         <Hero/>
+        <Footer/>
         </>
     )
 }
