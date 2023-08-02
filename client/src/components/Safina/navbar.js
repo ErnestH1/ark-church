cc8b791bec51372aa67ddd8d37c7cc798e3bb3e0
casey <caseyochieng46@gmail.com>
2023-08-02 12:03:46 +0300

    new navbar

--- client/src/components/Safina/navbar.js
+++ client/src/components/Safina/navbar.js
@@ -5,7 +5,7 @@ import NavLinks from './NavLinks';
 
 function Navbar() {
 
-const [open, setOpen] =useState(false)
+    const [open, setOpen] = useState(false)
 
     return (
         <nav className='flex justify-between'>
@@ -17,11 +17,11 @@ const [open, setOpen] =useState(false)
                 <NavLinks />
             </ul>
 
-            {/* Mobile navigation */}
-            <ul className='fixed top-0 z-50 bg-white w-2/3 h-screen shadow-2xl md:hidden flex flex-col gap-10 text-medium p-7 pt-20 right-0 '>
+            {/* Mobile navigation Start */}
+            <ul className={`fixed top-0 z-50 bg-white w-2/3 h-screen shadow-2xl md:hidden flex flex-col gap-10 text-medium p-7 pt-20 duration-500 ${open ? ' right-0' : 'right-[-100%]'}`}>
                 <NavLinks />
             </ul>
-            {/* Mobile navigation */}
+            {/* Mobile navigation End */}
         </nav>
     )
 }
