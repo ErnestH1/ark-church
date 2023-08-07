4f3638e6c52ad74c6e67fbcdef45e97d90403700
ErnestH1 <hansonernest7@gmail.com>
2023-08-07 17:18:58 +0300

    updated models

--- api/app/models.py
+++ api/app/models.py
@@ -26,7 +26,7 @@ class Video(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     description = Column(String)
-
+    file_path = Column(String)
 
 class Podcast(Base):
     __tablename__ = "podcasts"
